86d760c481a499aed7551e0748c6304b16acafe4
lucasklaassen <lucasklaassen1@gmail.com>
2020-04-01 15:07:43 -0700

    feat: run fullstory outside of angular for performance boost

--- projects/ngx-fullstory/src/lib/fullstory/fullstory.ts
+++ projects/ngx-fullstory/src/lib/fullstory/fullstory.ts
@@ -1,28 +1,36 @@
-import { Inject, Injectable, PLATFORM_ID, Optional } from '@angular/core';
-import { Router } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
+import {
+  Inject,
+  Injectable,
+  PLATFORM_ID,
+  Optional,
+  NgZone
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { isPlatformBrowser } from "@angular/common";
 
-import { FullstoryConfig } from '../shared/fullstory-config';
-import { Any, BootInput } from '../types/boot-input';
+import { FullstoryConfig } from "../shared/fullstory-config";
+import { Any, BootInput } from "../types/boot-input";
 
 /**
  * A provider with every Fullstory.JS method
  */
 export class Fullstory {
-
   private id: string;
 
   constructor(
     @Inject(FullstoryConfig) private config: FullstoryConfig,
     @Inject(PLATFORM_ID) protected platformId: object,
-    @Optional() @Inject(Router) private router: Router
+    @Optional() @Inject(Router) private router: Router,
+    private zone: NgZone
   ) {
     if (!isPlatformBrowser(this.platformId)) {
       return;
     }
 
-    // Run load and attach to window
-    this.loadFullstory(config);
+    this.zone.runOutsideAngular(() => {
+      // Run load and attach to window
+      this.loadFullstory(config);
+    });
 
     // // Subscribe to router changes
     // if (config && config.updateOnRouterChange) {
@@ -58,35 +66,56 @@ export class Fullstory {
 
   loadFullstory(config: FullstoryConfig): void {
     const args = arguments;
-    const win = (window as any);
+    const win = window as any;
 
-    win['_fs_debug'] = false;
-    win['_fs_host'] = 'fullstory.com';
-    win['_fs_org'] = config.fsOrg;
-    win['_fs_namespace'] = 'FS';
+    win["_fs_debug"] = false;
+    win["_fs_host"] = "fullstory.com";
+    win["_fs_org"] = config.fsOrg;
+    win["_fs_namespace"] = "FS";
 
     const load = (m: any, n: any, e: any, t: any, l: any) => {
-        if (e in m) {
-          if (m.console && m.console.log) {
-            m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');
-          }
-          return;
+      if (e in m) {
+        if (m.console && m.console.log) {
+          m.console.log(
+            'FullStory namespace conflict. Please set window["_fs_namespace"].'
+          );
         }
-        const g: any = m[e] = (a: any, b: any) => { g.q ? g.q.push([a, b]) : g._api(a, b); };
-        g.q = [];
-        let o: any = n.createElement(t);
-        o.async = 1;
-        o.src = 'https://' + win['_fs_host'] + '/s/fs.js';
-        const y: any = n.getElementsByTagName(t)[0]; y.parentNode.insertBefore(o, y);
-        g.identify = (i: any, v: any) => { g(l, { uid: i }); if (v) g(l, v) };
-        g.setUserVars = (v: any) => { g(l, v); };
-        g.shutdown = () => { g("rec", !1); };
-        g.restart = () => { g("rec", !0); };
-        g.consent = (a: any) => { g("consent", !args.length || a); };
-        g.identifyAccount = (i: any, v: any) => { o = 'account'; v = v || {}; v.acctId = i; g(o, v); };
-        g.clearUserCookie = () => { };
+        return;
+      }
+      const g: any = (m[e] = (a: any, b: any) => {
+        g.q ? g.q.push([a, b]) : g._api(a, b);
+      });
+      g.q = [];
+      let o: any = n.createElement(t);
+      o.async = 1;
+      o.src = "https://" + win["_fs_host"] + "/s/fs.js";
+      const y: any = n.getElementsByTagName(t)[0];
+      y.parentNode.insertBefore(o, y);
+      g.identify = (i: any, v: any) => {
+        g(l, { uid: i });
+        if (v) g(l, v);
+      };
+      g.setUserVars = (v: any) => {
+        g(l, v);
+      };
+      g.shutdown = () => {
+        g("rec", !1);
+      };
+      g.restart = () => {
+        g("rec", !0);
+      };
+      g.consent = (a: any) => {
+        g("consent", !args.length || a);
+      };
+      g.identifyAccount = (i: any, v: any) => {
+        o = "account";
+        v = v || {};
+        v.acctId = i;
+        g(o, v);
+      };
+      g.clearUserCookie = () => {};
     };
 
-    load(win, document, win['_fs_namespace'], 'script', 'user');
+    load(win, document, win["_fs_namespace"], "script", "user");
   }
 }
